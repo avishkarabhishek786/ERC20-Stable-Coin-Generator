1c47b438ba86bbed4a501a2d501f0ca82f93ac5a
Abhishek Sinha <avishkarabhishek786@gmail.com>
2020-12-23 22:06:37 +0530

    added token exchange

---
+++ contracts/TokenExchange.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
+
+import './VINC.sol';
+
+contract TokenExchange {
+    
+    VINC public token1;
+    VINC public token2;
+    
+    string public name = 'ERC20 Tokens Instant Exchange';
+    
+    event TokensPurchased(
+        VINC coin,
+        address sender,
+        address recipient,
+        uint amount
+    );
+    
+    constructor(VINC _token1, VINC _token2) public {
+        token1 = _token1;
+        token2 = _token2;
+    }
+    
+    function _safeTransferFrom(
+        VINC token,
+        address sender,
+        address recipient,
+        uint amount
+     ) private {
+        bool sent = token.transferFrom(sender, recipient, amount);
+        require(sent, "Token transfer failed");
+        emit TokensPurchased(token, sender, recipient, amount);
+     }
+     
+     function swap(address owner1, uint amount1, address owner2, uint amount2) public {
+        require(msg.sender == owner1 || msg.sender == owner2, "Not authorized");
+        require(owner1 != address(0) || owner2 != address(0), "Swapping to the zero address");
+        require(amount1 > 0 || amount2 > 0, "Swapping to zero amount");
+        require(
+            token1.allowance(owner1, address(this)) >= amount1,
+            "Token 1 allowance too low"
+        );
+        require(
+            token2.allowance(owner2, address(this)) >= amount2,
+            "Token 2 allowance too low"
+        );
+        
+        require(
+            token1.expected_receiving_tokens(owner2) == amount1,
+            "Token 1 expected amount too low"
+        );
+
+        require(
+            token2.expected_receiving_tokens(owner1) == amount2,
+            "Token 2 expected amount too low"
+        );
+        
+        _safeTransferFrom(token1, owner1, owner2, amount1);
+        _safeTransferFrom(token2, owner2, owner1, amount2);
+        
+     }
+       
+}
--- contracts/VINC.sol
+++ contracts/VINC.sol
@@ -9,6 +9,8 @@ contract VINC is ERC20Pausable, Ownable {
 
     address private vinc_owner;
     
+    mapping (address => mapping (address => uint256)) private _expected_receiving_tokens;
+    
     constructor(string memory _name, string memory _symbol, uint256 _initialSupply) public ERC20(_name, _symbol) {
         _mint(msg.sender, _initialSupply);
         vinc_owner = msg.sender;
@@ -30,6 +32,22 @@ contract VINC is ERC20Pausable, Ownable {
     
     function start() public {
         super._unpause();
+    }
+    
+    // Set a minimum amount you must receive from exchange address in a trade
+    function set_expected_receiving_tokens(address buyer, uint256 amount) internal {
+        require(vinc_owner != address(0), "ERC20: trading from the zero address");
+        require(buyer != address(0), "ERC20: trading to the zero address");
+
+        _expected_receiving_tokens[vinc_owner][buyer] = amount;
+        emit Set_receiving_tokens(vinc_owner, buyer, amount);
+    }
+    
+    event Set_receiving_tokens(address indexed owner, address indexed buyer, uint256 value);
+    
+    // amount that owner should get from buyer in a trade
+    function expected_receiving_tokens(address buyer) public view returns (uint256) {
+        return _expected_receiving_tokens[vinc_owner][buyer];
     }
     
 }
